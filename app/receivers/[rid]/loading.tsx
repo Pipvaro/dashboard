6338b9ba745f18826966480eae12392461e78e15
Wolfgang Preinfalk <w.preinfalk@outlook.de>
2025-09-18 08:31:37 +0200

    added better loading

--- app/receivers/[rid]/loading.tsx
+++ app/receivers/[rid]/loading.tsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+
 export default function Loading() {
-  return <div className="p-6 text-gray-400">Loading receiver…</div>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="animate-pulse">
+        <Image
+          src="/assets/Transparent/logo-beta.svg"
+          alt="Loading..."
+          width={180}
+          height={180}
+        />
+      </div>
+    </div>
+  );
 }
